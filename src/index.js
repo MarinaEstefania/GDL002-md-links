872c142bd5723b2d91be76352e2d4abddb1cb1a2
s5tefany <m.garciac11@gmail.com>
2019-04-15 20:12:13 -0500

    first try module.exports, require

---
+++ src/index.js
@@ -0,0 +1,3 @@
+const mdLinks = require('../src/md-links');
+
+console.log(mdLinks.add(1,2));
---
+++ src/md-links.js
@@ -0,0 +1,9 @@
+const mdLinks ={};
+
+const add = (x1, x2) => {
+return x1+x2;
+}
+
+mdLinks.add = add;
+
+module.exports = mdLinks;
