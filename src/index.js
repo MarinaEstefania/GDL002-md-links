b388734b3af59446200e9f54c372513ada24d80f
s5tefany <m.garciac11@gmail.com>
2019-04-30 10:56:37 -0500

    add functions readFile, findLink, putLinksInArray validateLinksFromArray

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-//import example
+/* //import example
 const path = require('path');
 const fs = require('fs');
 
-console.log(path.basename('34r2'));
+console.log(path.basename('example'));
+ */
+
+const fs = require('fs'); //import FileSystem module
+
+const filePathFile = (filePath, options) => {
+  return new Promise((resolve, reject)=> {
+    fs.readFile(filePath, function(err, data){
+      if (err){
+        return reject(err);
+      }
+      resolve(data.toString());
+    });
+  });
+};
+
+module.exports = filePathFile
--- src/md-links.js
+++ src/md-links.js
@@ -1,4 +1,4 @@
-//chmod +x md-links.js
+/* //chmod +x md-links.js
 
 const mdLinks ={};
 //function define if it is an Absolute or a relative Path
@@ -22,3 +22,58 @@ return x1+x2;
 mdLinks.add = add;
 
 module.exports = mdLinks;
+
+module.exports = {
+    add
+    isItAnAbsolutePath
+    3
+} */
+
+const mdLinks = require('./index.js');
+
+const filePath = process.argv[2];
+
+const resultReadFile  = mdLinks(filePath, null);
+
+resultReadFile
+    .then(
+        (data)=> {
+            const linkArray = parseLinks(data); //modifiq aqui
+            const objArray = linkArray.map(extactData); // modifiq aquí 
+            console.log(objArray);
+        }
+    ).catch(
+        (err)=> {
+            console.error(err);
+        }
+    );
+
+//Funcion para encontrar links en el archivo que leyó
+const parseLinks = (data) => {
+    const regExp = /\[(.+)\]\((.+)\)/g;
+    const matchLinks = data.match(regExp);
+    return matchLinks;
+};
+
+
+// Funcion que crea el objeto con texto y los enlaces
+const extactData = (mdLink) => {
+    const regExp = /\[(.+)\]\((.+)\)/g;
+    const obj = {};
+    const groups = regExp.exec(mdLink);  // matching groups
+    // 0 [Node.js](https://nodejs.org/en/)'
+    // 1 Node.js
+    // 2 https://nodejs.org/en/
+
+    obj.text = groups[1];
+    obj.link = groups[2];
+    return obj;
+};
+
+// Funcion que valida que la url sea un link valido
+const validateLinks = (link) => {
+   const regExp = /(?:https?:\/\/)?(?:www\.)?[a-z0-9-]+\.[a-z]{2,5}/g
+   const validLink = link.match(regExp);
+   return validLink;
+}
+
