8f584af7d2bc9d92f786b29d2e74168d21c0a4ad
s5tefany <m.garciac11@gmail.com>
2019-05-06 21:56:05 -0500

    validate-stats function working

--- src/md-links.js
+++ src/md-links.js
@@ -4,81 +4,129 @@ const process = require('process');
 const chalk = require('chalk');
 
 //Find Links in File
-const findLinksInFile = (data) => {
-    const regExp = /\[(.+)\]\((.+)\)/g;
-    const matchLinks = data.match(regExp);
-    return matchLinks;
+const findLinksInFile = data => {
+  const regExp = /\[(.+)\]\((.+)\)/g;
+  const matchLinks = data.match(regExp);
+  return matchLinks;
 };
-
 
 // Create an objetc and put Links in an Array
-const putLinksInArray = (mdLink) => {
-    const regExp = /\[(.+)\]\((.+)\)/g;
-    const obj = {};
-    const groups = regExp.exec(mdLink);  // matching groups
-    // 0 [Node.js](https://nodejs.org/en/)'
-    // 1 Node.js
-    // 2 https://nodejs.org/en/
+const putLinksInArray = mdLink => {
+  const regExp = /\[(.+)\]\((.+)\)/g;
+  const obj = {};
+  const groups = regExp.exec(mdLink); // matching groups
+  // 0 [Node.js](https://nodejs.org/en/)'
+  // 1 Node.js
+  // 2 https://nodejs.org/en/
 
-    obj.text = groups[1];
-    obj.link = groups[2];
-    return obj;
+  obj.text = groups[1];
+  obj.link = groups[2];
+  return obj;
 };
 
-// Check status of every link using Library 'node-fetch'
-const checkStatus = (res) => {
-    if (res.ok) { // res.status >= 200 && res.status < 300
-        return console.log(chalk.green(`✔`,  res.status, 'This link is working OK: ') + res.url );
-    }
-    return console.log(chalk.red(`✖`, res.status, 'This link is broken: ') + res.url);
-}
+// Check VALIDATE of every link using Library 'node-fetch'
+const checkValidate = res => {
+  if (res.ok) {
+    // res.status >= 200 && res.status < 300
+    
+    return console.log(chalk.green(`✔`, res.status, 'This link is working OK: ') + res.url);
+  }
+  return console.log(chalk.red(`✖`, res.status, 'This link is broken: ') + res.url);
+};
 
-//function to Check link status
-const fetchLinksStatus = (url) =>{
+//function to Check link VALIDATE
+const fetchLinksValidate = url => {
     fetch(url)
-        .then(checkStatus)
-        .catch(
-            (err)=> {
-                console.error(err);
-            }
-        );
-    }
-
-    //This part allows the user set options
-    const filePath = process.argv[2];
-    const optionSelected = process.argv[3];
-
-    const resultReadFile = mdLinks(filePath, optionSelected);
+    .then(checkValidate)
+    .catch(err => {
+        console.error(err);
+    });
+};
 
 
-    // Function to make validate option work
-    const validateOption = () => {
-        resultReadFile
-        .then(data => {
-            const linkArray = findLinksInFile(data);
-            const objArray = linkArray.map(putLinksInArray);
-            return objArray;})
-        .then(data => {
-            console.log(chalk.underline('Make an HTTP request to figure out if the links are working or not'));
+let statusOK = 0;
+let statusFail = 0;
+let total = 0
+//function to count links fail or OK
+const fetchLinksStatus = url => {
+  fetch(url)
+    .then(data => {
+      if (data.statusText != 'OK') {
+        // res.status >= 200 && res.status < 300
+        statusFail = statusFail + 1;
+      } else {
+        statusOK = statusOK + 1;
+      }
+      total = total + 1;
+      console.log('Total Links: ' + total);
+      console.log('Working Links :' + statusOK);
+      console.log('Broken Links: ' + statusFail);
+      console.log(' ');
+
+      return (total, statusOK, statusFail)
+    })
+    .catch(err => {
+      console.error(err);
+    });
+};
+
+//This part allows the user set options
+const filePath = process.argv[2];
+const optionSelected = process.argv[3];
+
+const resultReadFile = mdLinks(filePath, optionSelected);
+
+// Function to make validate option work
+const validateOption = () => {
+  resultReadFile
+    .then(data => {
+      const linkArray = findLinksInFile(data);
+      const objArray = linkArray.map(putLinksInArray);
+      return objArray;
+    })
+    .then(data => {
+      console.log(
+        chalk.underline('Make an HTTP request to figure out if the links are working or not'),
+      );
+      data.forEach(element => {
+        return fetchLinksValidate(element.link);
+      });
+    })
+    .catch(err => {
+      console.error(err);
+    });
+};
+
+//function to make validate-stats work
+const statsCount = () => {
+    resultReadFile
+    .then(data => {
+      const linkArray = findLinksInFile(data);
+      const objArray = linkArray.map(putLinksInArray);
+      return objArray;
+    })
+    .then(data => {
+      console.log(chalk.underline('Print text with count of total, working and broken links'));
             data.forEach(element => {
-            return fetchLinksStatus(element.link);
-            });
-        })
-        .catch(err => {
-            console.error(err);
-        });
-    }
-
-    //function to make NO OPTION work
-    const noOptionSelected = () =>{
-              resultReadFile.then(data => {
-                const linkArray = findLinksInFile(data);
-                const objArray = linkArray.map(putLinksInArray);
-                console.log(chalk.underline('Printing found links and their title from the file given'));
-                console.log(objArray);
-                return objArray;
-              });
+                return fetchLinksStatus(element.link);
+            })
+    })
+    .catch(err => {
+      console.error(err);
+    });
 }
+//function to make NO OPTION work
+const noOptionSelected = () => {
+  resultReadFile.then(data => {
+    const linkArray = findLinksInFile(data);
+    const objArray = linkArray.map(putLinksInArray);
+    console.log(chalk.underline('Printing found links and their title from the file given'));
+    console.log(objArray);
+    return objArray;
+  });
+};
+
+
 //This part goes to the chosen option using a SWITCH
 const flowDirection = setDirection => {
   switch (setDirection) {
@@ -89,25 +137,23 @@ const flowDirection = setDirection => {
 
     case 'stats':
       console.log(chalk.bold('stats option selected'));
-      console.log(chalk.underline('Working On It...'));
+      statsCount();
       break;
 
     case 'validate-stats':
       console.log(chalk.bold('validate&stats options selected'));
-      console.log(chalk.underline('Working On It...'));
+      statsOptionSelected()
       break;
 
     default:
-    console.log(chalk.bold('NO option selected'));
-    noOptionSelected();
+      console.log(chalk.bold('NO option selected'));
+      noOptionSelected();
   }
 };
 
 flowDirection(optionSelected);
 
-
-
-  /*   fetch('https://httpbin.org/status/400')
+/*   fetch('https://httpbin.org/status/400')
   .then(res => {
         console.log(res.ok);
         console.log(res.status);
@@ -115,7 +161,7 @@ flowDirection(optionSelected);
     });
 */
 
-                /* const flowDirection = (setDirection) => {
+/* const flowDirection = (setDirection) => {
                     if(setDirection == 'validate') {
                        return console.log('validate option')
                     }
